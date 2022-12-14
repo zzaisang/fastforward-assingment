0ab5b870649c82ce48eb074aaeedf5f955522a46
zzai_sang <zzai_sang@naver.com>
2022-12-14 19:56:57 +0900

    Repository Test 추가

---
+++ src/test/java/com/example/fastforwardassignment/repository/RepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.fastforwardassignment.repository;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@TestPropertySource("classpath:application-test.yml")
+public class RepositoryTest {
+}
